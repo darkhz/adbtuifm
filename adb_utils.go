darkhz/adbtuifm

commit bfcf4c78d68114613de375797c5772a261542105

ui: Allow changing to any directory not in current path
This commit introduces a floating inputfield which allows for entering
any path and immediately changing the directory to said path, as long as
the path is valid

local_utils.go
@@ -83,6 +83,18 @@ func trimPath(testPath string, cdBack bool) string {
 	return testPath
 }
 
+func checkPath(testPath string) bool {
+	fi, err := os.Stat(testPath)
+	if err != nil {
+		return false
+	}
+	if !fi.Mode().IsDir() {
+		return false
+	}
+
+	return true
+}
+
 func (p *dirPane) ChangeDir(cdFwd bool, cdBack bool) {
 	row := p.row
 	testPath := p.path
@@ -109,6 +121,10 @@ func (p *dirPane) ChangeDir(cdFwd bool, cdBack bool) {
 	list := p.pathList
 
 	p.path = filepath.ToSlash(trimPath(testPath, false))
+
+	if list == nil {
+		return
+	}
 
 	sort.Slice(list, func(i, j int) bool {
 		if list[i].Mode.IsDir() != list[j].Mode.IsDir() {

ui.go
@@ -162,6 +162,8 @@ func setupPane(selPane *dirPane, auxPane *dirPane) {
 			}
 			selPane.ChangeDir(false, false)
 			auxPane.ChangeDir(false, false)
+		case 'G':
+			selPane.showChangeDirInput()
 		case 'q':
 			stopApp()
 		}
@@ -200,6 +202,31 @@ func showConfirmModal(msg string, Dofunc func()) {
 
 	pages.AddAndSwitchToPage("modal", modal(msgbox, 80, 29), true).ShowPage("main")
 	app.SetFocus(msgbox)
+}
+
+func (p *dirPane) showChangeDirInput() {
+	input := tview.NewInputField()
+	input.SetTitle("Change Directory to:")
+	input.SetTitleAlign(tview.AlignCenter)
+	input.SetBorder(true)
+
+	input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch event.Key() {
+		case tcell.KeyEscape:
+			pages.SwitchToPage("main")
+			app.SetFocus(prevPane.tbl)
+		case tcell.KeyEnter:
+			pages.SwitchToPage("main")
+			app.SetFocus(prevPane.tbl)
+			p.path = trimPath(input.GetText(), false)
+			p.ChangeDir(false, false)
+		}
+
+		return event
+	})
+
+	pages.AddAndSwitchToPage("modal", modal(input, 80, 3), true).ShowPage("main")
+	app.SetFocus(input)
 }
 
 func modal(p tview.Primitive, width, height int) tview.Primitive {

commit 61274e9569d85363d84a14881ef35525e19afdea

ui: Fix buggy behaviour in ChangeDirInput
Fixes: bfcf4c78d68114613de375797c5772a261542105

adb_utils.go
@@ -167,29 +167,30 @@ func (o *opsWork) adbOps() {
 	}
 }
 
-func (p *dirPane) adbListDir(testPath string) {
+func (p *dirPane) adbListDir(testPath string) bool {
 	client, device := getAdb()
 	if client == nil || device == nil {
-		return
+		return false
 	}
 
 	_, err := device.Stat(testPath)
 	if adb.HasErrCode(err, adb.ErrCode(adb.FileNoExistError)) {
 		showError(statError, testPath)
-		return
+		return false
 	} else if err != nil {
 		showError(unknownError, testPath)
-		return
+		return false
 	}
 
 	if p.pathList != nil && !p.isDir(testPath) {
-		return
+		showError(unknownError, testPath)
+		return false
 	}
 
 	dent, err := device.ListDirEntries(testPath)
 	if err != nil {
 		showError(statError, testPath)
-		return
+		return false
 	}
 
 	for dent.Next() {
@@ -211,8 +212,10 @@ func (p *dirPane) adbListDir(testPath string) {
 		p.pathList = append(p.pathList, ent)
 	}
 	if dent.Err() != nil {
-		return
+		return false
 	}
+
+	return true
 }
 
 func isSymDir(testPath, name string) bool {

local_utils.go
@@ -29,15 +29,15 @@ func (p *dirPane) isDir(testPath string) bool {
 	return true
 }
 
-func (p *dirPane) localListDir(testPath string) {
+func (p *dirPane) localListDir(testPath string) bool {
 	fi, err := os.Lstat(testPath)
 	if err != nil {
-		return
+		return false
 	}
 
 	file, err := os.Open(testPath)
 	if err != nil {
-		return
+		return false
 	}
 	defer file.Close()
 
@@ -52,7 +52,7 @@ func (p *dirPane) localListDir(testPath string) {
 
 		fi, err = os.Stat(testPath + name)
 		if err != nil {
-			return
+			return false
 		}
 
 		mode := fi.Mode()
@@ -67,6 +67,8 @@ func (p *dirPane) localListDir(testPath string) {
 
 		p.pathList = append(p.pathList, &d)
 	}
+
+	return true
 }
 
 func trimPath(testPath string, cdBack bool) string {
@@ -83,21 +85,10 @@ func trimPath(testPath string, cdBack bool) string {
 	return testPath
 }
 
-func checkPath(testPath string) bool {
-	fi, err := os.Stat(testPath)
-	if err != nil {
-		return false
-	}
-	if !fi.Mode().IsDir() {
-		return false
-	}
-
-	return true
-}
-
 func (p *dirPane) ChangeDir(cdFwd bool, cdBack bool) {
 	row := p.row
 	testPath := p.path
+	origPath := p.path
 
 	if cdFwd && p.pathList != nil && !p.isDir(testPath) {
 		return
@@ -113,17 +104,26 @@ func (p *dirPane) ChangeDir(cdFwd bool, cdBack bool) {
 
 	switch p.mode {
 	case mAdb:
-		p.adbListDir(testPath)
+		origPath = p.apath
+		cdFwd = p.adbListDir(testPath)
 	case mLocal:
-		p.localListDir(filepath.FromSlash(testPath))
+		origPath = p.dpath
+		cdFwd = p.localListDir(filepath.FromSlash(testPath))
 	}
 
 	list := p.pathList
 
-	p.path = filepath.ToSlash(trimPath(testPath, false))
-
-	if list == nil {
+	if list == nil && !cdFwd {
+		p.path = filepath.ToSlash(trimPath(origPath, false))
 		return
+	}
+
+	p.path = filepath.ToSlash(trimPath(testPath, false))
+	switch p.mode {
+	case mAdb:
+		p.apath = p.path
+	case mLocal:
+		p.dpath = p.path
 	}
 
 	sort.Slice(list, func(i, j int) bool {

ui.go
@@ -209,6 +209,7 @@ func (p *dirPane) showChangeDirInput() {
 	input.SetTitle("Change Directory to:")
 	input.SetTitleAlign(tview.AlignCenter)
 	input.SetBorder(true)
+	input.SetText(p.path)
 
 	input.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		switch event.Key() {
@@ -218,6 +219,14 @@ func (p *dirPane) showChangeDirInput() {
 		case tcell.KeyEnter:
 			pages.SwitchToPage("main")
 			app.SetFocus(prevPane.tbl)
+
+			switch p.mode {
+			case mAdb:
+				p.apath = p.path
+			case mLocal:
+				p.dpath = p.path
+			}
+
 			p.path = trimPath(input.GetText(), false)
 			p.ChangeDir(false, false)
 		}