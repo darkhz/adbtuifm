bd0aa6bce3863804f5e2966fa46dcb7c504efa00
darkhz <kmachanwenw@gmail.com>
2021-07-21 15:55:02 +0530

    handler: Merge delete case into move/copy case
    There are very minor differences between these cases, so merge them into
    one case and account for the differences.

--- handler.go
+++ handler.go
@@ -6,11 +6,11 @@ import (
 )
 
 var (
-	ops       opsMode
-	opPaths   []string
-	srcPath  string
+	ops     opsMode
+	opPaths []string
+	srcPath string
 	dstPath string
-	opsLock   bool
+	opsLock bool
 )
 
 func modeSwitchHandler(pane *dirPane) {
@@ -32,46 +32,43 @@ func modeSwitchHandler(pane *dirPane) {
 }
 
 func opsHandler(selPane *dirPane, auxPane *dirPane, key rune) {
-	row, _ := selPane.tbl.GetSelection()
-
 	switch key {
-	case 'c', 'm':
+	case 'c', 'm', 'd':
 		if opsLock {
 			return
 		}
 
-		srcPath = selPane.path + selPane.tbl.GetCell(row, 0).Text
+		selection := selPane.tbl.GetCell(selPane.row, 0).Text
+		srcPath = filepath.Join(selPane.path, selection)
 
 		if key == 'm' {
 			ops = opMove
-		} else {
+		} else if key == 'c' {
 			ops = opCopy
+		} else {
+			ops = opDelete
 		}
 
-		auxPane.tbl.SetSelectable(true, true)
-		selPane.tbl.SetSelectable(false, false)
-		app.SetFocus(auxPane.tbl)
+		if ops == opMove || ops == opCopy {
+			opsLock = true
 
-		opsLock = true
-		return
+			app.SetFocus(auxPane.tbl)
+			auxPane.tbl.SetSelectable(true, true)
+			selPane.tbl.SetSelectable(false, false)
+
+			return
+		}
+
+		dstPath = ""
+		selPane.tbl.SetSelectable(true, true)
+		auxPane.tbl.SetSelectable(false, false)
 	case 'p':
 		if !opsLock {
 			return
 		}
 
-		dstPath = filepath.Join(selPane.path, path.Base(srcPath))
 		opsLock = false
-	case 'd':
-		if opsLock {
-			return
-		}
-
-		auxPane.tbl.SetSelectable(false, false)
-		selPane.tbl.SetSelectable(true, true)
-
-		srcPath = selPane.path + selPane.tbl.GetCell(row, 0).Text
-		dstPath = ""
-		ops = opDelete
+		dstPath = filepath.Join(selPane.path, path.Base(srcPath))
 	}
 
 	showOpConfirm(ops, srcPath, dstPath, func() {
